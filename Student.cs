6388ee8981c3bcbf4a676bc5b645f3f6c744effe
Nambu Kamali <nambukamali@gmail.com>
2026-02-16 20:27:01 +0530

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentManagementAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Course { get; set; } = string.Empty;
+        public string Year { get; set; } = string.Empty;
+    }
+}
