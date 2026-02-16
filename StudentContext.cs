eba9f66943666e8527df1bb69555c4e7fb149d65
Nambu Kamali <nambukamali@gmail.com>
2026-02-16 20:26:12 +0530

    Add files via upload

---
+++ StudentContext.cs
@@ -0,0 +1,14 @@
+﻿using StudentManagementAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+namespace StudentManagementAPI.Data
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
