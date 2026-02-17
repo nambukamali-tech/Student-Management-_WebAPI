d84cbeda1db06cd095a185a099bb7d700dba44bc
Nambu Kamali <nambukamali@gmail.com>
2026-02-17 22:24:08 +0530

    Add files via upload

---
+++ JoinsController.cs
@@ -0,0 +1,48 @@
+﻿using EmployeeManagement_API.Data;
+using EmployeeManagement_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JoinsController : ControllerBase
+    {
+        private readonly EmployeeContext _context;
+        public JoinsController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Attendance>>> AddAttendance(Attendance attendance)
+        {
+            _context.Attendances.Add(attendance);
+            await _context.SaveChangesAsync();
+            return Ok(attendance);
+        }
+
+
+        [HttpGet("report")]
+        public async Task<ActionResult<List<Attendance>>> AttendanceReport(int EmployeeId)
+        {
+            var report = await _context.Attendances
+                .Include(e => e.Employee)
+                .Select(e => new
+                {
+                    e.EmployeeId,
+                    EmployeeName = e.Employee.EmployeeName,
+                    e.AttendanceId,
+                    e.AttendanceDate,
+                    e.Status
+
+                }).ToListAsync();
+            return Ok(report);
+                
+
+        }
+
+
+    }
+}
