b8f407dadf1516f2b842295d9590a9f058cd61e7
Nambu Kamali <nambukamali@gmail.com>
2026-02-19 22:49:20 +0530

    Add files via upload

---
+++ EmployeeContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeManagement_API.Models;
+namespace EmployeeManagement_API.Data
+
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options
+            ) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+       
+    }
+}
