303a13b7a78f58799801e63f4d9c505969cbb470
Nambu Kamali <nambukamali@gmail.com>
2026-02-19 22:49:41 +0530

    Add files via upload

---
+++ Attendance.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EmployeeManagement_API.Models
+{
+    public class Attendance
+    {
+        public int AttendanceId { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime AttendanceDate { get; set; }
+        public string Status { get; set; } = string.Empty;
+        [JsonIgnore]
+        public Employee? Employee { get; set; }
+
+    }
+}
