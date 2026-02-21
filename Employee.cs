821977e51c66f21c9804b806496bcf5c5923d6a5
Nambu Kamali <nambukamali@gmail.com>
2026-02-21 22:24:18 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeManagement_API.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; } = string.Empty;
+        public string EmployeeRole { get; set; } = string.Empty;
+        public string Experience { get; set; } = string.Empty;
+        public List<Attendance> Attendances { get; set; } = new List<Attendance>();
+
+    }
+}
