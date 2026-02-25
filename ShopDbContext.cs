f0ef383c92f709bf4c124f7aa38a7b7f2fae4845
Nambu Kamali <nambukamali@gmail.com>
2026-02-25 20:23:12 +0530

    Add files via upload

---
+++ ShopDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CRUD_Operations_Practice2.Models;
+namespace CRUD_Operations_Practice2.Data
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Shop> Shop { get; set; }
+    }
+}
